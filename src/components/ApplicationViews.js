1e93d94b6705a5b672358c609c16331164f0f1bf
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-09-09 11:37:34 -0500

    created file structure

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,12 @@
+import {Route, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+
+class ApplicationViews extends Component {
+    render() {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
+export default ApplicationViews
---
+++ src/components/JavaJuice.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ApplicationViews from "./ApplicationViews"
+import "./JavaJuice.css"
+
+class JavaJuice extends Component {
+    render() {
+        return (
+            <ApplicationViews />
+        );
+    }
+}
+
+export default JavaJuice
