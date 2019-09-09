49b4f04d356eca71e7185056730e70e4607b2038
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-09-09 15:56:41 -0500

    completed login and registration

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,13 +2,36 @@ import {Route, Redirect} from "react-router-dom";
 import React, { Component } from "react";
 import Login from "./auth/Login"
 import Register from "./auth/Register"
+import Home from "./home/Home"
 
 class ApplicationViews extends Component {
+    isAuthenticated = () => sessionStorage.getItem("credentials") !== null
     render() {
-        return (
-            <p>Hello World</p>
-        )
+      return (
+        <React.Fragment>
+          <Route exact path ="/login" render={props => {
+            if (this.isAuthenticated()) {
+              return <Redirect to="/" />
+            } 
+            return <Login {...props} />
+          }}/>
+          <Route exact path ="/register" render={props => {
+            if (this.isAuthenticated()) {
+              return <Redirect to="/" />
+            } 
+            return <Register {...props} />
+          }}/>
+          <Route path ="/" render={props => {
+              if (this.isAuthenticated()) {
+                return (
+                <Home {...props} />
+                )
+              } 
+              return <Redirect to="/login" />
+            }}/>
+        </React.Fragment>
+      );
     }
-}
+  }
 
 export default ApplicationViews
